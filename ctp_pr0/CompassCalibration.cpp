d2996f5a2bdc07907a1685e4100131cb61d66a13
jli127 <jian.d.li@intel.com>
2012-04-09 19:37:17 +0800

    LEX: change MAG_MAX filter for compass calibration
    BZ: 30161
    
    This patch change MAG_MAX to a smaller value
    to drop big magnetic data from calibration.
    
    Big magnetic data usually means device is
    in strong manetic field and the data read
    from sensor is not accurate.
    
    Change-Id: I596c38a2e8210e8b9d84174d3668eb37ce37b988
    Signed-off-by: jli127 <jian.d.li@intel.com>
    Reviewed-on: http://android.intel.com:8080/42797
    Reviewed-by: Liu, Hong <hong.liu@intel.com>
    Reviewed-by: Wang, Zhifeng <zhifeng.wang@intel.com>
    Reviewed-by: Du, Alek <alek.du@intel.com>
    Tested-by: Wang, Zhifeng <zhifeng.wang@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- ctp_pr0/CompassCalibration.cpp
+++ ctp_pr0/CompassCalibration.cpp
@@ -4,7 +4,7 @@
 #include "CompassCalibration.h"
 
 /* Earth magnetic parameters */
-#define MAG_MAX 80.0f /* 80 mt*/
+#define MAG_MAX 60.0f /* 60 mt*/
 #define MAG_MIN 25.0f /* 25 mt*/
 #define MAG_MAX_INC 10.0f /* 10 mt */
 
